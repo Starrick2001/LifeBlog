b40415b6bdc5081c332a7f983d88bdfe15f420a2
Lê Bùi Dĩ Hoà <lebuidihoa257@gmail.com>
2021-06-08 12:13:27 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "member";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "member";
+$servername = "remotemysql.com";
+$username = "dwNSRsKHFD";
+$password = "9j86ae7qYs";
+$database = "dwNSRsKHFD";
 $connect = new mysqli($servername, $username, $password, $database);
 
 if ($connect->connect_error) {
