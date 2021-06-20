48f7064d15348dd87069db77abb7918c906f37e3
Lê Bùi Dĩ Hoà <lebuidihoa257@gmail.com>
2021-06-21 03:31:48 +0700

    fixed some bugs

--- function/dangky.php
+++ function/dangky.php
@@ -6,7 +6,7 @@ if (isset($_POST["email"]) && isset($_POST['name']) && isset($_POST['birth']) &&
     $birth = $_POST['birth'];
     $password = md5($_POST['password']);
     $reEnterPassword = md5($_POST["reEnterPassword"]);
-    $avatarUrl = "img/Logo/Logo1.png";
+    $avatarUrl = "img/avatar/Logo1.png";
     $addmemeber = "INSERT INTO member (email, name, birth, password, avatarUrl)
             VALUES ('{$email}', '{$name}', '{$birth}', '{$password}', '{$avatarUrl}')";
     if ($connect->query($addmemeber) === TRUE) {
